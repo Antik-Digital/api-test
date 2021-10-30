d902c04fdac82e21759d7ff889a932ccda2d5486
Alan Pikholtz <aupikholtz@gmail.com>
2021-10-30 19:37:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 /* eslint-disable new-cap */
 require('dotenv').config();
 
-const personas = [
-  {
-    'id': 1,
-    'nombre': 'alan',
-    'apellido': 'pikholtz',
-  },
-  {
-    'id': 2,
-    'nombre': 'alan',
-    'apellido': 'antar',
-  },
-];
 
 const express = require('express');
 const cors = require('cors');
@@ -40,27 +28,27 @@ router.route('/users/:id').get((request, response) => {
   });
 });
 
-app.get('/', (request, response) => {
-  DBUsers.getUsers().then((result) => {
-    response.json(result);
-  });
-});
+// app.get('/', (request, response) => {
+//   DBUsers.getUsers().then((result) => {
+//     response.json(result);
+//   });
+// });
 
 
-app.get('/api/people/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const persona = personas.find((persona) => persona.id == id);
-  if (persona) {
-    response.json(persona);
-  } else {
-    response.status(404).end();
-  }
-});
+// app.get('/api/people/:id', (request, response) => {
+//   const id = Number(request.params.id);
+//   const persona = personas.find((persona) => persona.id == id);
+//   if (persona) {
+//     response.json(persona);
+//   } else {
+//     response.status(404).end();
+//   }
+// });
 
 
-app.get('/api/people', (request, response) => {
-  response.json(personas);
-});
+// app.get('/api/people', (request, response) => {
+//   response.json(personas);
+// });
 
 app.use((request, response, next) =>{
   console.log(request.path);
